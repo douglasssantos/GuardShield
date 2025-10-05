19f4372596465f20cf63c5930586908d1df6e789
Douglas S. Santos <douglassantos2127@gmail.com>
2025-10-05 00:47:53 -0300

    fix migration

--- database/migrations/2024_01_01_211442_create_guard_shield_permissions_table.php
+++ database/migrations/2024_01_01_211442_create_guard_shield_permissions_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
         Schema::create('guard_shield_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('module_id')->constrained("guard_shield_permissions_modules")->onDelete('cascade');
+            $table->foreignId('module_id')->nullable()->constrained("guard_shield_permissions_modules")->nullOnDelete('cascade');
             $table->string("key")->nullable();
             $table->string("name");
             $table->string('description')->nullable();
